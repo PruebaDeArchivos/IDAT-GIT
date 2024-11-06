5bf8e6fec1db4a26f1e8b150dbb8c1bcebeefad1
root <root@DESKTOP-7PKUFBS>
2024-11-05 21:35:39 -0500

    Saaaa

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ var nombre = prompt("Cual es tu nombre?")
 var nameDom = document.getElementById("nameDom")
 nameDom.textContent = "Hola " +  nombre
 //console.log(nombre)
+
+var Saaa
+
+var Saaa
+var Saaa
+var Saaa
+var Saaa
+
+
